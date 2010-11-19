3347347c1295d66ff839dab14e1c76edc3720d02
AYUkawa,Yasuyuki <ayu+github@yamayo.to>
2010-11-19 15:40:45 +0900

    Modified buildout.cfg, Added unitTest.py

---
+++ unitTest.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import gaeunit
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    # import sys;sys.argv = ['', 'Test.testName']
+
+    unittest.main()
+
