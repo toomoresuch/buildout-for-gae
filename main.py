94df14d5e456237480e1f060a4b4937e7c74c10b
AYUkawa,Yasuyuki <ayu+github@yamayo.to>
2010-11-22 17:22:18 +0900

    Modified app.yaml, buildout.cfg and main.py

--- main.py
+++ main.py
@@ -3,12 +3,25 @@
 import sys
 sys.path.insert(0, './distlib.zip')
 
+import re
 from itty import *
+
+target = re.compile(r'ja')
 
 
 @get('/')
+def accept_lang(request):
+    ja = str(request._environ.get('HTTP_ACCEPT_LANGUAGE'))
+
+    if target.match(ja):
+        raise Redirect('/ja/')
+    else:
+        raise Redirect('/en/')
+
+
 def index(request):
     return 'Hello World!'
+
 
 run_itty(server='appengine')
 
