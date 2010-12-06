6f49d433c7189522070aabd6b289877583a14b60
AYUkawa,Yasuyuki <ayu+github@yamayo.to>
2010-12-06 13:26:33 +0900

    Changed Base Web Framework, from kay to tipfy.

--- unitTest.py
+++ unitTest.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from google.appengine.ext import db
-from kay.app import get_application
-from kay.ext.testutils.gae_test_base import GAETestBase
-from werkzeug import BaseResponse, Client, Request
+from tipfy import RequestHandler, Response, Rule, Tipfy, default_config
+from apps.hello_world.handlers import HelloWorldHandler
 
+import gae_test_base
 import unittest
 
+gae_test_base.APP_ID = default_config['app_id']
 
-class TestCase(GAETestBase):
+
+class TestCase(gae_test_base.GAETestBase):
 
     # USE_PRODUCTION_STUBS = True
     # USE_REMOTE_STUBS = True
@@ -16,16 +17,19 @@ class TestCase(GAETestBase):
     CLEANUP_USED_KIND = True
 
     def setUp(self):
-
-        app = get_application()
-        self.client = Client(app, BaseResponse)
+        app = Tipfy(rules=[Rule('/', endpoint='home',
+                    handler=HelloWorldHandler)])
+        self.client = app.get_test_client()
 
     def tearDown(self):
         pass
 
     def test(self):
         response = self.client.get('/')
-        assert response.status_code is 200
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(response.data, 'Hello, World!')
 
 
+if __name__ == '__main__':
+    unittest.main()
 
